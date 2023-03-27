e72be69d174d0c7d5e8551778c8162263d5bf290
zainabmohammed <zzainabmohammed6@gmail.com>
2023-03-27 17:39:43 +0300

    counter task

---
+++ src/component/counter.jsx
@@ -0,0 +1,35 @@
+
+import React , {useState} from "react";
+
+function Counter(){
+       
+   
+        const [count,setCount]=useState(0)
+     
+
+        function handeldecrement(){
+        
+          setCount(count-1)
+   
+        }
+        function handelIncrement(){
+        
+            setCount(count+1)
+     
+          }
+        
+        return (
+
+            <div>
+                <button onClick={handeldecrement}>decrease</button>
+                <span>{count}</span>
+                <button onClick={handelIncrement}>increase</button>
+            </div>
+
+
+
+        );
+    }
+
+ 
+export default Counter;
